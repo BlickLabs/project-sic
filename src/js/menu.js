db4764a57b75dbd3d349eaccfc806851821edeb7
Raúl Marty <santor3medio@gmail.com>
2018-05-14 10:03:20 -0500

    #2. Add the configuration to the scrolling navbar and hamburger menu to the script file menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,42 +1,111 @@
-(function () {
-  var mobile = window.matchMedia('(max-width: 876px)'); // Change
+var last_known_scroll_position = 0;
+var ticking = false;
 
-  function detectTouch(e) {
-    if (!$(e.target).is('.site-navbar__menu-trigger') && !$(e.target).is('.site-navbar__menu-container') && !$(e.target).closest('.site-navbar__menu-container').length && $('.site-navbar__menu-container').hasClass('site-navbar__menu-container--show-menu')) {
-      e.preventDefault();
-      $('.site-navbar__menu-container').removeClass('site-navbar__menu-container--show-menu');
-      $('body').removeClass('noscroll');
-    }
-  }
+var siteNavbar = document.querySelector('.navbar');
+var navSeparator = document.querySelector('.navbar-separator');
 
-  function detectClick() {
-    if ($('.site-navbar__menu-container').hasClass('site-navbar__menu-container--show-menu')) {
-      $('.site-navbar__menu-container').removeClass('site-navbar__menu-container--show-menu');
-      $('body').removeClass('noscroll');
+//navbar-fixed-top
+function doSomething(scroll_pos) {
+  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-sic/index.html' || window.location.pathname === '/project-sic/') {
+    if (scroll_pos > 30) {
+      siteNavbar.classList.add('navbar-scroll');
+      $('.navbar-logo').removeClass('show-icon');
+      $('.navbar-logo-scroll').addClass('show-icon');
+      navSeparator.style.backgroundColor = "#5275a7";
+      $('.navbar-sections-title').addClass('navbar-sections-scroll');
+      $('.navbar-dropdown-icon-scroll').addClass('show-icon');
+      $('.navbar-dropdown-icon').removeClass('show-icon');
+      $('.navbar-hamburger-icon-scroll').addClass('show-icon');
+      $('.navbar-hamburger-icon').removeClass('show-icon');
+
     } else {
-      $('.site-navbar__menu-container').addClass('site-navbar__menu-container--show-menu');
-      $('body').addClass('noscroll');
+      siteNavbar.classList.remove('navbar-scroll');
+      $('.navbar-logo').addClass('show-icon');
+      $('.navbar-logo-scroll').removeClass('show-icon');
+      navSeparator.style.backgroundColor = "#ffffff";
+      $('.navbar-sections-title').removeClass('navbar-sections-scroll');
+      $('.navbar-dropdown-icon-scroll').removeClass('show-icon');
+      $('.navbar-dropdown-icon').addClass('show-icon');
+      $('.navbar-hamburger-icon-scroll').removeClass('show-icon');
+      $('.navbar-hamburger-icon').addClass('show-icon');
     }
   }
+}
 
-  if (mobile.matches) {
-    $('body')[0].addEventListener('touchstart', detectTouch, {passive: false});
-    $('.site-navbar__menu-trigger').click(detectClick);
-  } else {
-    $('body')[0].removeEventListener('touchstart', detectTouch, {passive: false});
-    $('.site-navbar__menu-trigger').off('click');
-    $('body').removeClass('noscroll');
+doSomething();
+
+window.addEventListener('scroll', function(e) {
+  if (e.view) {
+    last_known_scroll_position = e.view.pageYOffset;
   }
+  else {
+    last_known_scroll_position = window.scrollY;
+  }
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false
+    });
+  }
+  ticking = true;
+});
 
-  $(window).resize(function () {
-    $('body')[0].removeEventListener('touchmove', detectTouch, {passive: false});
-    $('.site-navbar__menu-trigger').off('click');
-    $('body').removeClass('noscroll');
-    $('.site-navbar__menu-container').removeClass('show-menu');
+window.addEventListener('scroll', function(e) {
+  if (e.view) {
+    last_known_scroll_position = e.view.pageYOffset;
+  }
+  else {
+    last_known_scroll_position = window.scrollY;
+  }
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false
+    });
+  }
+  ticking = true;
+});
 
-    if (mobile.matches) {
-      $('body')[0].addEventListener('touchmove', detectTouch, {passive: false});
-      $('.site-navbar__menu-trigger').click(detectClick);
-    }
-  });
-})();
+function showMenu() {
+  document.querySelector('#menuContainer').style.width = '85%';
+  document.querySelector('#hamburgerMenu').style.display = 'none';
+}
+
+function hideMenu() {
+  document.querySelector('#menuContainer').style.width = '0%';
+  document.querySelector('#hamburgerMenu').style.display = 'inline';
+}
+
+window.addEventListener('click', function(e) {
+  if (!e.target.dataset.hasOwnProperty('attribute')) {
+    hideMenu();
+  }
+});
+
+/*Events of touchstart to mobile version*/
+
+var showHamburger = document.querySelector('#hamburgerMenu');
+
+showHamburger.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  showMenu();
+});
+
+var hideHamburger = document.querySelector('#hideHamburger');
+
+hideHamburger.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  hideMenu();
+});
+
+/* Events of click to desktop version*/
+
+showHamburger.addEventListener('click', function(e) {
+  e.preventDefault();
+  showMenu();
+});
+
+hideHamburger.addEventListener('click', function(e) {
+  e.preventDefault();
+  hideMenu();
+});
