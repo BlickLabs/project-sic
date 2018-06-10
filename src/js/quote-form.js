13af58fb7e6f6b48dfd9fab8792604a15d6dcecf
Raúl Marty <santor3medio@gmail.com>
2018-06-09 21:20:59 -0500

    #23. Add the integrations for the english version of quote form in quote-form.js file.

--- src/js/quote-form.js
+++ src/js/quote-form.js
@@ -5,7 +5,7 @@ $('form[name="sic-quote-form"]').validate({
   messages: {
    name: "Por favor, introduce tu nombre",
    email: "Por favor introduce tu correo electrónico",
-   company: "Por favor introduce tu correo electrónico",
+   company: "Por favor selleciona una solución",
   },
   submitHandler: function(form) {
    var data = $('form[name="sic-quote-form"]').serialize();
@@ -26,5 +26,36 @@ $('form[name="sic-quote-form"]').validate({
    var errors = validator.numberOfInvalids();
    alertify.logPosition("bottom right");
    alertify.error("Verifica la información. Tienes " + errors + " errores.");
+  }
+});
+
+$('form[name="sic-quote-form-en"]').validate({
+  name: 'required',
+  email: 'required',
+  company: 'required',
+  messages: {
+   name: "Please, enter your name",
+   email: "Please, enter your email",
+   company: "Please, select a solution",
+  },
+  submitHandler: function(form) {
+   var data = $('form[name="sic-quote-form-en"]').serialize();
+   console.log(data);
+   $.ajax({
+     url: 'http://integrations.blick.mx/sic/quote/form/',
+     method: 'POST',
+     data: data
+   }).done(function(data) {
+     if (parseInt(data) === 1) {
+       alertify.logPosition("bottom right");
+       alertify.success("The email has been sent. Thanks for contact us!");
+       $('form[name="sic-quote-form-en"]')[0].reset();
+     }
+   });
+  },
+  invalidHandler: function(event, validator) {
+   var errors = validator.numberOfInvalids();
+   alertify.logPosition("bottom right");
+   alertify.error("Verify yout information. You have " + errors + " errors.");
   }
 });
