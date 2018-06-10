0124475c303bf7d0250faeefa393871a0893260e
Raúl Marty <santor3medio@gmail.com>
2018-06-09 21:20:17 -0500

    #23. Add the integrations for the english version of contact form in contact-form.js file.

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -26,3 +26,32 @@ $('form[name="sic-contact-form"]').validate({
    alertify.error("Verifica la información. Tienes " + errors + " errores.");
   }
 });
+
+$('form[name="sic-contact-form-en"]').validate({
+  name: 'required',
+  email: 'required',
+  messages: {
+   name: "Please, enter your name",
+   email: "Please, enter your email",
+  },
+  submitHandler: function(form) {
+   var data = $('form[name="sic-contact-form-en"]').serialize();
+   console.log(data);
+   $.ajax({
+     url: 'http://integrations.blick.mx/sic/contact/form/',
+     method: 'POST',
+     data: data
+   }).done(function(data) {
+     if (parseInt(data) === 1) {
+       alertify.logPosition("bottom right");
+       alertify.success("The email has been sent. Thanks for contact us!");
+       $('form[name="sic-contact-form-en"]')[0].reset();
+     }
+   });
+  },
+  invalidHandler: function(event, validator) {
+   var errors = validator.numberOfInvalids();
+   alertify.logPosition("bottom right");
+   alertify.error("Verify yout information. You have " + errors + " errors.");
+  }
+});
