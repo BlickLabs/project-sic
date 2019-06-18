e14aa062763019c256bcab7efda31cc16db42637
Raúl Marty <santor3medio@gmail.com>
2019-06-18 15:44:07 -0500

    #43. English version from homepage and services pages.

--- src/js/home-logos-carousel.js
+++ src/js/home-logos-carousel.js
@@ -1,26 +1,13 @@
-// var logosHomeCarousel = $('.home-clients-carousel');
-
-// logosHomeCarousel.owlCarousel ({
-//   loop: true,
-//   nav: true,
-//   dots: true,
-//   navText: ['<img class="solutions-navigation-icon" src="img/icons/icon-arrow-left.png" />', '<img class="solutions-navigation-icon" src="img/icons/icon-arrow-right.png" />'],
-  
-// });
-
-// logosHomeCarousel.each(function(index) {
-//   $(this).find('.owl-nav, .owl-dots').wrapAll("<div class='solutions-navigation-controls'></div>");
-// });
-
 var logosHomeCarousel = $('.home-clients-carousel');
 
 logosHomeCarousel.owlCarousel({
-  loop: false,
+  loop: true,
   center: false,
+  autoplay: true,
+  autoplayTimeout:3000,
   autoWidth: true,
   margin: 0,
-  nav: true,
+  nav: false,
   dots: false,
   navText: ['<img class="navigation-icon" src="img/icons/arrow-blue-left.png" />', '<img class="navigation-icon" src="img/icons/arrow-blue-right.png" />'],
-  
 })
